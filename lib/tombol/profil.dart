ab92bb0f005af6b35dae016a1c36338b03c16675
byeone001 <halimqolbun@gmail.com>
2025-09-25 14:33:41 +0700

    new

--- lib/tombol/profil.dart
+++ lib/tombol/profil.dart
@@ -143,7 +143,7 @@ class _BagianAtas extends StatelessWidget {
                     image: DecorationImage(
                       fit: BoxFit.cover,
                       image: NetworkImage(
-                        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                        'assets/LOGO.png',
                       ),
                     ),
                   ),
